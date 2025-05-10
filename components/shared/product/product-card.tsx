b66486cb0faf58b6144c423705d3556768357b13
Youssef Ahmed <youssefelnaggar2001@outlook.com>
2025-05-11 01:09:56 +0300

    Add the Prisma ORM and create the Prodcts table

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any; }) => {
     return ( 
@@ -24,7 +25,7 @@ const ProductCard = ({ product }: { product: any; }) => {
                 <div className="flex-between gap-4">
                     <p>{product.rating} Stars</p>
                     { product.stock > 0 ? (
-                        <p className="font-bold">{product.price}$</p>
+                        <ProductPrice value={Number(product.price)} /> 
                     ) : (
                         <p className="text-red-500">Out of Stock</p>
                     )}
--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -1,5 +1,15 @@
-const ProdctPrice = ( {value, className }: { value: number; className?: string }) => {
-    return (  );
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ( {value, className }: { value: number; className?: string }) => {
+
+    const stringValue = value.toFixed(2);
+    const [intValue, floatValue] = stringValue.split('.');
+    return ( <p className={cn('text-2xl', className)}>
+        <span className="text-xs align-super">$</span>
+        {intValue}
+        <span className="text-xs align-super">.{floatValue}</span>
+
+    </p> );
 }
  
-export default ProdctPrice;
+export default ProductPrice;
